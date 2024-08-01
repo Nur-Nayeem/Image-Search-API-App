e12a4e1de62e9bdf49e6aa1ed817d5a182da014f
Nur-Nayeem <nurnayem768@gmail.com>
2024-08-01 14:51:08 +0600

    Image search Api App

---
+++ script.js
@@ -0,0 +1,91 @@
+const accessKey = "xAUjGIc1psYIfHCJkvcOVsiE0NBG8MPR1hwbRzjhBXc"
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-reults");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+let pageFrnt = 1;
+
+async function randomImage(){
+    const url = `https://api.unsplash.com/search/photos?page=${pageFrnt}&query=random&client_id=${accessKey}`;
+    const response = await fetch(url)
+    const data =await response.json();
+    const results = data.results;
+    if(pageFrnt === 1){
+        searchResults.innerHTML = "";
+
+    }
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+    pageFrnt++;
+    if(pageFrnt>1){
+        showMore.style.display = "block";
+    }
+}
+
+randomImage();
+
+async function searchImages(){
+    inputData = inputEl.value;
+    console.log(inputData);
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const response = await fetch(url)
+    const data =await response.json();
+    const results = data.results;
+    if(page === 1){
+        searchResults.innerHTML = "";
+
+    }
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+    page++;
+    if(page>1){
+        showMore.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit",(event) =>{
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click",() =>{
+    if(inputEl.value !== ""){
+        searchImages();
+    }
+    else
+    {
+        randomImage();
+    }
+    
+});
